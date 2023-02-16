40bc1bc280162140896357b173935d443d77f162
KaiOsU8 <kokokovachev10@gmail.com>
2023-02-16 23:18:13 +0100

    commit

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 // Hieronder staan de logingegevens van de database
 $dbHost = 'localhost';
 $dbName = 'php-pdo-crud-2209a';
-$dbUser = 'kik_2209a';
-$dbPass = 'qQ7jx8jgM(Or.SQk';
+$dbUser = 'root';
+$dbPass = '';
